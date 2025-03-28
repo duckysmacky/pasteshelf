3968386e9036ae03aec259b45bbd7d8a4808bd46
DuckySmacky <kriperoka@gmail.com>
2025-03-29 02:22:38 +0300

    Set up project structure

---
+++ src/main/java/io/github/duckysmacky/pasteshelf/PasteshelfApplication.java
@@ -0,0 +1,13 @@
+package io.github.duckysmacky.pasteshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteshelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteshelfApplication.class, args);
+    }
+
+}
