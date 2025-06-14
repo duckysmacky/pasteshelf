3118b6fe55040fea85948c924b03bb7478be4749
DuckySmacky <kriperoka@gmail.com>
2025-06-15 00:32:37 +0300

    Add user registration

---
+++ src/main/java/io/github/duckysmacky/pasteshelf/web/controllers/AuthController.java
@@ -0,0 +1,27 @@
+package io.github.duckysmacky.pasteshelf.web.controllers;
+
+import io.github.duckysmacky.pasteshelf.application.services.UserService;
+import io.github.duckysmacky.pasteshelf.infrastructure.models.User;
+import io.github.duckysmacky.pasteshelf.web.dto.RegisterUserRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegisterUserRequest request) {
+        User user = userService.registerUser(request.getUsername(), request.getPassword(), request.getEmail());
+        return ResponseEntity.ok("User " + user.getUsername() + " was successfully registered");
+    }
+
+}
---
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    email VARCHAR NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL
+);
